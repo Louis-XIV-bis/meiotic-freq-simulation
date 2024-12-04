530407967f6ab6fa52a4b5c6476f5865ab003068
louis ollivier <louisxiv@clust-slurm-client.ifb.local>
2024-12-04 10:29:33 +0100

    no gaussian data + script + plot

---
+++ figures/supp/variable_vs_fixed_alpha/fix_var_alpha.R
@@ -0,0 +1,122 @@
+#!/usr/bin/env Rscript
+
+## Author : Louis OLLIVIER ~ louis.ollivier@universite-paris-saclay.fr
+## Université Paris-Saclay
+## Lab : LISN ~ UMR9015 ~ BIOINFO team 
+
+###### Package initialization  ----------------------------------------
+
+if (!require('ggplot2', quietly = T)) install.packages('ggplot2');
+if (!require('readr', quietly = T)) install.packages('readr');
+if (!require('tibble', quietly = T)) install.packages('tibble');
+if (!require('dplyr', quietly = T)) install.packages('dplyr');
+if (!require('sjPlot', quietly = T)) install.packages('sjPlot');
+if (!require('RColorBrewer', quietly = T)) install.packages('RColorBrewer');
+if (!require('cowplot', quietly = T)) install.packages('cowplot');
+
+library(ggplot2)
+library(readr)
+library(tibble)
+library(dplyr)
+library(sjPlot)
+library(RColorBrewer)
+library(cowplot)
+
+########################################################################
+# Function that upload and format the data
+
+rm(list=ls())
+
+# Subset data for ctrl and neutral condition and for alpha = 0.01 and 1
+data_var = read_csv('../../../data/pi_merged.csv') %>%
+  filter((s == 0.05 & h == 0.5 & rho == '5e-08')) %>% 
+  filter(window != 'chr2') %>%   
+  mutate(alpha = as.factor(1 / GR)) %>%
+  filter(alpha %in% c(0.01, 1)) %>% 
+  mutate(ymin = mean - sd, ymax = mean + sd) %>% 
+  mutate(window = as.numeric(window) / 1000000)
+data_var
+
+data_fix = read_csv('pi_nogaussian_alpha.csv') %>%
+  filter((s == 0.05 & h == 0.5 & rho == '5e-08') | (s == 0)) %>% 
+  filter(window != 'chr2') %>%   
+  mutate(alpha = as.factor(1 / GR)) %>%
+  filter(alpha %in% c(0.01, 1)) %>% 
+  mutate(ymin = mean - sd, ymax = mean + sd) %>% 
+  mutate(window = as.numeric(window) / 1000000)
+data_fix
+
+########################################################################
+# Create plots individually #
+
+# Explicitly set the y-axis limits to be the same for both plots
+y_axis_limits <- c(-1e-5, 8e-05)
+
+# Create color palette 
+colors <- c("#1B9E77","#E7298A")
+pi_fix = data_fix %>%
+  ggplot(aes(x = window, y = mean, group = alpha)) +
+  geom_ribbon(aes(ymin = ymin, ymax = ymax, fill = alpha), alpha = 0.2) +
+  geom_line(aes(color = alpha)) +
+  scale_color_manual(values = colors) +
+  scale_fill_manual(values = colors) +
+  labs(x = "Sequence position (Mbp)",
+          y = expression(pi),
+          color = expression(alpha),
+          fill = expression(alpha),
+          linetype = expression(alpha)) + 
+  theme_light() + 
+  theme(
+    axis.title.x = element_text(size = 20),
+    axis.title.y = element_text(size = 20),
+    axis.text.x = element_text(size = 18),
+    axis.text.y = element_text(size = 18)
+    ) +
+  guides(color = "none", fill = "none") +
+  scale_y_continuous(limits = y_axis_limits)
+pi_fix
+
+
+pi_var = data_var %>%
+   ggplot(aes(x = window, y = mean, group = alpha)) +
+   geom_ribbon(aes(ymin = ymin, ymax = ymax, fill = alpha), alpha = 0.2) +
+   geom_line(aes(color = alpha)) +
+   scale_color_manual(values = colors) +
+   scale_fill_manual(values = colors) +
+   labs(x = "Sequence position (Mbp)",
+        y = expression(pi),
+        color = expression(alpha),
+        fill = expression(alpha),
+        linetype = expression(alpha)) + 
+   theme_light() + 
+   theme(
+       axis.title.x = element_text(size = 20),
+       axis.title.y = element_blank(),
+      legend.title = element_text(size = 22),
+       legend.text = element_text(size = 22),
+       axis.text.x = element_text(size = 18),
+       axis.text.y = element_blank()
+     ) + 
+   scale_y_continuous(limits = y_axis_limits)
+pi_var
+
+# Arrange the plots side by side using plot_grid
+combined_plot <- plot_grid(pi_fix, pi_var + theme(plot.margin = margin(l = 23)),
+                           labels = "AUTO",
+                           label_size = 20,
+                           ncol = 2,
+                           align = "h",  # Horizontal alignment
+                           rel_widths = c(1, 1)  # Adjust relative widths to account for the legend
+)
+combined_plot
+
+# Adding labels 
+plot = ggdraw() +
+  draw_plot(combined_plot, width = 1, height = 1) +
+  draw_text("Fixed intervals", x = 0.29, y = 0.92, size = 18, color = "black") +
+  draw_text("Variable intervals", x = 0.71, y = 0.92, size = 18, color = "black")
+plot
+
+ggsave("fix_var_alpha.png", plot = combined_plot, width = 16, height = 6, units = "in")
+
+rm(list=ls())
---
+++ script/nogaussian_alpha/tree_sumstats.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+## Université Paris-Saclay
+## Lab : LISN ~ UMR9015 ~ BIOINFO team 
+
+# This program will use the .trees files (= tree sequence, TS) given by SLiM to compute summary statistics (pi)
+
+import msprime, pyslim, tskit, numpy as np, pandas as pd, sys, warnings
+
+rep = int(sys.argv[1]) # Replicate number
+n = int(sys.argv[2]) # Number of sample for the simplication and sumstats (diploïd) 
+ne = int(sys.argv[3]) # Size of the population 
+mu = float(sys.argv[4]) # Mutation rate
+rho = float(sys.argv[5]) # Recombination rate
+nb_windows = int(sys.argv[6]) 
+GR = int(sys.argv[7])
+
+##### SAMPLING, ADDING NEUTRAL MUTATION AND GETTING SNP MATRIX #####
+##### RECAPITATE, SAMPLING AND ADDING NEUTRAL MUTATION) ###################################
+# https://tskit.dev/pyslim/docs/latest/tutorial.html
+
+ts = tskit.load(f"results/ts_{rep}_{GR}.trees")
+ts = pyslim.update(ts)
+
+warnings.simplefilter('ignore', msprime.TimeUnitsMismatchWarning)
+
+# Recapitate the simulation to provide a “prior history” for the initial generation of the simulation = fully coalesced #
+# RTS : recapitated 
+
+rts = pyslim.recapitate(ts, recombination_rate=rho*GR, ancestral_Ne=ne)
+      
+# Sampling indivuals from the TS to compute sumstats later # 
+# SRTS = simplified recapitated TS 
+
+rng = np.random.default_rng()
+alive_inds = pyslim.individuals_alive_at(rts, 0) # sample individuals alive = coming from the last generation
+keep_indivs = rng.choice(alive_inds, n, replace=False)
+keep_nodes = []
+for i in keep_indivs:
+  keep_nodes.extend(rts.individual(i).nodes)
+  
+srts = rts.simplify(keep_nodes, keep_input_roots=True)
+
+# Adding neutral mutations to the samples (overlay) # 
+# OSRTS = overlay simplified recapitated TS 
+
+osrts = msprime.mutate(srts, rate=(mu), keep=True) # keep existing mutations
+
+##### SUMMARY STATISTICS (WITH WINDOWS) ####################################
+
+# We want a user defined number of windows for the first chromosome but only 1 value
+# for the second since it's entirely neutral 
+
+windows_chr = np.linspace(0, osrts.sequence_length/2, num=nb_windows)
+windows_chr = [int(x) for x in windows_chr] + [int(osrts.sequence_length)]
+
+## PI ##
+pi = osrts.diversity(windows=windows_chr, mode='site')
+pi_chr1 = pi[:-1]
+pi_chr2 = pi[-1]
+
+pi_chr1.tofile(f"results/pi_chr1_{rep}_{GR}.csv", sep=',')
+pi_chr2.tofile(f"results/pi_chr2_{rep}_{GR}.csv", sep=',')
